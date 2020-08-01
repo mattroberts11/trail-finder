714bee72de723e59627676455b1bb74a25a35945
Matt Roberts <justskiit@gmail.com>
2020-08-01 16:29:01 -0600

    Firebase login and testing completed

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+
+import PropTypes from "prop-types";
+
+import { withRouter } from "react-router-dom";
+
+import { auth } from "../../firebase";
+
+import authentication from "../../services/authentication";
+
+import EmptyState from "../EmptyState";
+
+import { ReactComponent as CabinIllustration } from "../../illustrations/cabin.svg";
+import { ReactComponent as InsertBlockIllustration } from "../../illustrations/insert-block.svg";
+// import { ReactComponent as Logo } from "../../illustrations/TraiFinderLogo.png";
+
+class HomePage extends Component {
+  signInWithEmailLink = () => {
+    const { user } = this.props;
+
+    if (user) {
+      return;
+    }
+
+    const emailLink = window.location.href;
+
+    if (!emailLink) {
+      return;
+    }
+
+    if (auth.isSignInWithEmailLink(emailLink)) {
+      let emailAddress = localStorage.getItem("emailAddress");
+
+      if (!emailAddress) {
+        this.props.history.push("/");
+
+        return;
+      }
+
+      authentication
+        .signInWithEmailLink(emailAddress, emailLink)
+        .then((value) => {
+          const user = value.user;
+          const displayName = user.displayName;
+          const emailAddress = user.email;
+
+          this.props.openSnackbar(
+            `Signed in as ${displayName || emailAddress}`
+          );
+        })
+        .catch((reason) => {
+          const code = reason.code;
+          const message = reason.message;
+
+          switch (code) {
+            case "auth/expired-action-code":
+            case "auth/invalid-email":
+            case "auth/user-disabled":
+              this.props.openSnackbar(message);
+              break;
+
+            default:
+              this.props.openSnackbar(message);
+              return;
+          }
+        })
+        .finally(() => {
+          this.props.history.push("/");
+        });
+    }
+  };
+
+  render() {
+    const { user } = this.props;
+
+    if (user) {
+      return <EmptyState image={<CabinIllustration />} />;
+    }
+
+    return (
+      <EmptyState
+        image={<InsertBlockIllustration />}
+        title="Trail Finder"
+        description="Find cool trails nearbuy!"
+      />
+    );
+  }
+
+  componentDidMount() {
+    this.signInWithEmailLink();
+  }
+}
+
+HomePage.propTypes = {
+  user: PropTypes.object,
+};
+
+export default withRouter(HomePage);
